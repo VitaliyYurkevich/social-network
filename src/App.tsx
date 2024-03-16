a9136cbc42e7b7d53b473890c833052fb9a73c54
VitaliyYurkevich <vitaliy.yurkevich@yandex.ru>
2024-03-16 14:16:20 +0300

    add Login component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import './App.css';
-import {Header} from "./components/header/Header";
 import Navbar from "./components/navbar/Navbar";
 import {BrowserRouter, Route} from "react-router-dom";
 import Settings from "./components/settings/Settings";
 import Music from "./components/music/Music";
 import News from "./components/news/News";
-import {DialogsContainer} from "./components/dialogs/DialogsContainer";
 import {UsersContainer} from "./components/users/UsersContainer";
-import {ProfileContainer} from "./components/Profile/ProfileContainer";
 import HeaderContainer from "./components/header/HeaderContainer";
-
+import {Login} from "./components/login/Login";
+import DialogsContainer from "./components/dialogs/DialogsContainer";
+import ProfileContainer from "./components/Profile/ProfileContainer";
 
 
 function App() {
@@ -18,18 +17,18 @@ function App() {
 
 
     return (
-
         <BrowserRouter>
             <div className="app-wrapper">
-                <HeaderContainer />
+                <HeaderContainer  />
                 <Navbar/>
                 <div className={'app-wrapper-content'}>
                     <Route path='/dialogs' render={() => <DialogsContainer />}/>
-                    <Route path='/profile/:userId?' render={() => <ProfileContainer  />}/>
+                    <Route path='/profile/:userId?' render={() => <ProfileContainer />}/>
                     <Route path='/users' render={() => <UsersContainer />}/>
-                    <Route path='/news' component={News}/>
-                    <Route path='/music' component={Music}/>
-                    <Route path='/settings' component={Settings}/>
+                    <Route path='/news' component={News} />
+                    <Route path='/music' component={Music} />
+                    <Route path='/settings' component={Settings} />
+                    <Route path='/login' component={Login} />
                 </div>
             </div>
         </BrowserRouter>
