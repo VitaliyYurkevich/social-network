8778593df4fa4f4dfafd5af5c8a8e287cd851ec1
VitaliyYurkevich <vitaliy.yurkevich@yandex.ru>
2024-08-13 00:56:20 +0300

    update all

--- src/components/login/LoginForm.tsx
+++ src/components/login/LoginForm.tsx
@@ -3,6 +3,7 @@ import {Field, InjectedFormProps, reduxForm} from "redux-form";
 import {Input} from "../commons/formControls/FormsControls";
 import {required} from "../utils/validators/validators";
 import classes from "../commons/formControls/FormsControls.module.css"
+import styled from "styled-components";
 
 
 //типо как MapStateToProps
@@ -23,7 +24,10 @@ export const LoginForm: React.FC<InjectedFormProps<FormDataType>> = (props) => {
     return (
         <div>
             <form onSubmit={props.handleSubmit}>
-                vitaliy.yurkevich@yandex.ru
+                Данные тестового аккаунта:
+                Email: free@samuraijs.com
+
+                Password: free
                 <div>
                     <Field validate={[required]} placeholder={"email"} component={Input} name={"email"}/>
                 </div>
@@ -39,13 +43,26 @@ export const LoginForm: React.FC<InjectedFormProps<FormDataType>> = (props) => {
                 </div>
                {/* {props.  && <img src={props.active} />}*/}
                 <div>
-                    <button>Login</button>
+                    <StyledBtn>Login</StyledBtn>
                 </div>
             </form>
         </div>
     );
 };
-
+const StyledBtn = styled.button`
+  height: 30px;
+  width: 80px;
+  cursor: pointer;
+  border: 0;
+  background: rgba(1, 175, 199, 0.9);
+  box-shadow: 0 5px 15px 0 rgba(12, 226, 255, 0.9);
+  transition: 0.5s;
+  border-radius: 10px;
+  &:hover {
+    transform: translate(0, -3px);
+    box-shadow: 0 20px 40px 0 rgba(12, 226, 255, 0.9);
+  }
+`
 export const LoginReduxForm = reduxForm<FormDataType>({
     form: 'login'
 })(LoginForm)
